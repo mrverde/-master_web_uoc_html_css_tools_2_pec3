3848f6b5da167f9c747b94def30793247c32831a
Pepe Mateo <jdmatvaz@gmail.com>
2021-12-05 21:36:22 +0100

    Minor media queries home changes

--- src/assets/pages/PageHome/PageHome.js
+++ src/assets/pages/PageHome/PageHome.js
@@ -10,13 +10,13 @@ const PageHome = () => {
     }, []);
 
     return <>
-        <div className="absolute grid w-full h-full grid-cols-4 gap-4 pl-5 bg-cover align-right bg-home-image max-w-fullhd mxlg:bg-min60">
+        <div className="absolute grid w-full h-full grid-cols-4 gap-0 pl-5 bg-cover align-right bg-home-image max-w-fullhd mxlg:bg-min60">
+            <div className="mxmd:hidden" />
             <div />
             <div />
-            <div />
-            <div className="mr-8 bg-primary parallelepiped bg-opacity-70">
-                <div className="pt-5 pl-5 mx-auto text-xl text-white md:text-2xl xl:text-4xl 2xl:text-6xl 3xl:text-7xl sm:pl-10 md:pl-14 lg:pl-20 xl:pl-28 2xl:pl-32 3xl:pl-40 font-secondary">The <br />  Orphan <br />of Zhao </div>
-                <div className="pt-5 pl-5 mx-auto text-xl text-white md:text-md xl:text-xl 2xl:text-2xl 3xl:text-3xl sm:pl-10 md:pl-14 lg:pl-20 xl:pl-28 2xl:pl-32 3xl:pl-40 font-secondary">A yellow history</div></div>
+            <div className="mr-8 mxmd:col-span-2 bg-primary parallelepiped bg-opacity-70">
+                <div className="pt-5 pl-16 mx-auto text-2xl text-white sm:text-4xl md:text-2xl xl:text-4xl 2xl:text-6xl 3xl:text-7xl sm:pl-28 md:pl-14 lg:pl-20 xl:pl-28 2xl:pl-32 3xl:pl-40 font-secondary">The <br />  Orphan <br />of Zhao </div>
+                <div className="pt-5 pl-16 mx-auto text-xl text-white md:text-md xl:text-xl 2xl:text-2xl 3xl:text-3xl sm:pl-28 md:pl-14 lg:pl-20 xl:pl-28 2xl:pl-32 3xl:pl-40 font-secondary">A yellow history</div></div>
         </div>
     </>
 }
