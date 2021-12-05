d6854782a2889f8b3cf9a47df7448a50ee78abf4
Pepe Mateo <jdmatvaz@gmail.com>
2021-12-05 16:54:46 +0100

    Added characters page styles

--- src/assets/components/CardCharacter/CardCharacter.js
+++ src/assets/components/CardCharacter/CardCharacter.js
@@ -3,14 +3,14 @@ import React from "react";
 const CardCharacter = props => {
     const { img, imgAlt, characterName, actorName, chBack } = props;
 
-    return <div>
-        <div>
-            <img src={img} alt={imgAlt} />
+    return <div className="relative flex flex-col max-w-lg min-w-0 m-8 bg-white rounded shadow-md bg-clip-border">
+        <div className="inline-block">
+            <img className="w-full align-middle" src={img} alt={imgAlt} />
         </div>
-        <div>
-            <p>{characterName}</p>
-            <p>{actorName}</p>
-            <p>{chBack}</p>
+        <div className="p-5">
+            <p className="px-2 mt-0 text-2xl font-bold">{characterName}</p>
+            <p className="px-2 py-0 mt-0 mb-4 font-bold">{actorName}</p>
+            <p className="px-2 italic">{chBack}</p>
         </div>
     </div>
 }
--- src/assets/pages/PageCharacters/PageCharacters.js
+++ src/assets/pages/PageCharacters/PageCharacters.js
@@ -7,7 +7,7 @@ const PageCharacters = () => {
     const characterList = characters;
 
     return <>
-        <div>
+        <div className="flex flex-row flex-wrap justify-center pt-8 pb-20 mb-20">
             {characterList.map((char, idx) => (
                 <CardCharacter
                     key={`${char}-${idx}`}
